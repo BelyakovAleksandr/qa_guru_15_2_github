a2efe8734607d6e9793210c0ef68ce1a6fe8c8fa
BelyakovAleksandr <113695437+BelyakovAleksandr@users.noreply.github.com>
2022-10-03 21:35:35 +0400

    Тест на конфликт Дубль 2

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -3,15 +3,17 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest_0() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
     void exampleTest_1() {
-        Assertions.assertFalse(10 > 20);
+        Assertions.assertFalse(new Random().nextInt() > 2);
     }
 }
