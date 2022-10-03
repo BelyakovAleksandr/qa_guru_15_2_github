8502b950f38538b144a4435abf5634c63f9930ec
BelyakovAleksandr <113695437+BelyakovAleksandr@users.noreply.github.com>
2022-10-03 20:18:51 +0400

    New commit

---
+++ src/test/java/guru/qa/ExampleTest.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest_0() {
+        Assertions.assertTrue(2 >= 1);
+    }
+
+    @Test
+    void exampleTest_1() {
+        Assertions.assertFalse(10 >= 20);
+    }
+}
